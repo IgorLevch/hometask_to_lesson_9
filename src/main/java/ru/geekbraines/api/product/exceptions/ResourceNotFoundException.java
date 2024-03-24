0e32c2538d0856d93dd4b991b184492f9e10eddf
IgorLevch <igorlevvv@gmail.com>
2024-03-24 13:22:22 +0300

    Commit 3. Final Ready Project

--- src/main/java/ru/geekbraines/api/product/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbraines.api.product.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-
-}
